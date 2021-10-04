b6012ef17007545e9d71e184749de46048e63772
abosch-kr <anthony.bosch@kroger.com>
2021-10-04 10:36:05 -0400

    Added origin data member

--- app/src/main/java/com/example/marvelapp/Hero.kt
+++ app/src/main/java/com/example/marvelapp/Hero.kt
@@ -1,7 +1,8 @@
 package com.example.marvelapp
 
-class Hero (name: String, alterEgo: String, ability: String){
+class Hero (name: String, alterEgo: String, origin: String, ability: String){
     var name: String = name
     var alterEgo: String = alterEgo
+    var origin: String = origin
     var ability: String = ability
 }
