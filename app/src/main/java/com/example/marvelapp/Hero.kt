4636d59c5d3b4f4cd817baae11af1b377019869f
abosch-kr <anthony.bosch@kroger.com>
2021-10-04 09:22:54 -0400

    Changed data type

--- app/src/main/java/com/example/marvelapp/Hero.kt
+++ app/src/main/java/com/example/marvelapp/Hero.kt
@@ -3,7 +3,9 @@ package com.example.marvelapp
 data class Hero (
     private val name: String,
     private val alterEgo: String,
-    private val abilities: Array<String>
+    private var abilities: ArrayList<String>
 ){
-
+    init {
+        abilities = ArrayList()
+    }
 }
