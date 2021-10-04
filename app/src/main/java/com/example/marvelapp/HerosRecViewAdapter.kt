f8de8ac3365e51cd286ed0983aab3c5f0236eba1
abosch-kr <anthony.bosch@kroger.com>
2021-10-04 08:59:55 -0400

    First commit

--- app/src/main/java/com/example/marvelapp/HerosRecViewAdapter.kt
+++ app/src/main/java/com/example/marvelapp/HerosRecViewAdapter.kt
@@ -1,8 +1,4 @@
 package com.example.marvelapp
 
-data class HerosRecViewAdapter(
-    private val name: String,
-    private val alterEgo: String,
-    private val origin: String
-) {
+class HerosRecViewAdapter {
 }
