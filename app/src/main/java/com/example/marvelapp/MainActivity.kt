5147c68fda035d227f246354efc4a86a7af397a3
abosch-kr <anthony.bosch@kroger.com>
2021-10-06 13:18:39 -0400

    Updated logging

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var herosRecView: RecyclerView
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "Entering onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         herosRecView = findViewById(R.id.herosRecView)
         val heros: ArrayList<Hero> = ArrayList()
-        val adapter: HerosRecViewAdapter = HerosRecViewAdapter()
+        val adapter = HerosRecViewAdapter()
 
         heros.add(Hero("Iron Man", "Tony Stark","Long Island, New York", "Powered armor suit"))
         heros.add(Hero("Captain America", "Steven Rogers","Brooklyn, New York", "Enhanced strength"))
@@ -24,5 +27,6 @@ class MainActivity : AppCompatActivity() {
         adapter.setHeros(heros)
         herosRecView.adapter = adapter
         herosRecView.layoutManager = LinearLayoutManager(this)
+        Log.d(TAG, "Exiting onCreate")
     }
 }
