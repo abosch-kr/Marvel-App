03bf627a6251957ae8a2f277f7f50a12557712b2
abosch-kr <anthony.bosch@kroger.com>
2021-10-06 15:20:45 -0400

    Changed ArrayList to mutableListOf

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -8,25 +8,29 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var herosRecView: RecyclerView
-    private val TAG = "MainActivity"
+    private lateinit var heroesRecView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "Entering onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        herosRecView = findViewById(R.id.herosRecView)
-        val heros: ArrayList<Hero> = ArrayList()
-        val adapter = HerosRecViewAdapter()
+        heroesRecView = findViewById(R.id.heroes_rec_view)
+        val heroes = mutableListOf<Hero>()
+        val heroesAdapter = HeroesRecViewAdapter()
 
-        heros.add(Hero("Iron Man", "Tony Stark","Long Island, New York", "Powered armor suit"))
-        heros.add(Hero("Captain America", "Steven Rogers","Brooklyn, New York", "Enhanced strength"))
-        heros.add(Hero("Hulk", "Bruce Banner", "Dayton, Ohio","Superhuman strength"))
+        heroes.add(Hero("Iron Man", "Tony Stark","Long Island, New York", "Powered armor suit"))
+        heroes.add(Hero("Captain America", "Steven Rogers","Brooklyn, New York", "Enhanced strength"))
+        heroes.add(Hero("Hulk", "Bruce Banner", "Dayton, Ohio","Superhuman strength"))
 
-        adapter.setHeros(heros)
-        herosRecView.adapter = adapter
-        herosRecView.layoutManager = LinearLayoutManager(this)
+        heroesAdapter.setHeros(heroes as ArrayList<Hero>)
+        heroesRecView.apply {
+            adapter = heroesAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
         Log.d(TAG, "Exiting onCreate")
+    }
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
