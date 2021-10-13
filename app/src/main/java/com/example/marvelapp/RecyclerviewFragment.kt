709b9c4405978b9ef4c9ce735bd8359524b2c26c
abosch-kr <anthony.bosch@kroger.com>
2021-10-13 13:21:41 -0400

    Implemented onCreateView

--- app/src/main/java/com/example/marvelapp/RecyclerviewFragment.kt
+++ app/src/main/java/com/example/marvelapp/RecyclerviewFragment.kt
@@ -5,17 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RecyclerviewFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class RecyclerviewFragment : Fragment() {
+    private lateinit var heroesRecView: RecyclerView
+    private lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recyclerview, container, false)
+        val itemView = inflater.inflate(R.layout.fragment_recyclerview, container, false)
+        val heroesAdapter = HeroesRecViewAdapter()
+        layoutManager = LinearLayoutManager(context)
+        heroesRecView = itemView.findViewById(R.id.heroes_rec_view)
+        val heroes = mutableListOf<Hero>()
+
+        heroes.add(Hero("Iron Man", "Tony Stark","Long Island, New York", "Powered armor suit"))
+        heroes.add(Hero("Captain America", "Steven Rogers","Brooklyn, New York", "Enhanced strength"))
+        heroes.add(Hero("Hulk", "Bruce Banner", "Dayton, Ohio","Superhuman strength"))
+        heroes.add(Hero("Black Panther", "T'Challa", "Wakanda, Africa","Vibranium suit"))
+        heroes.add(Hero("Deadpool", "Bruce Banner", "Dayton, Ohio","Superhuman strength"))
+        heroes.add(Hero("Ant-Man", "Scott Lang", "New York","Size shifting"))
+        heroes.add(Hero("Wolverine", "James Howlett", "Canada","Claws"))
+        heroes.add(Hero("Vision", "Tom Smith", "Brooklyn, New York","Superhuman intelligence"))
+        heroes.add(Hero("Doctor Strange", "Stephen Vincent Strange", "New York","Mastery of magic"))
+        heroes.add(Hero("Hawkeye", "Clinton Barton", "Waverly, Iowa","Marksman"))
+        heroes.add(Hero("Thor", "Thor Odinson", "Asgard","Hammer"))
+        heroes.add(Hero("Anthony", "James Howlett", "Canada","Claws"))
+        heroes.add(Hero("Jared", "Tom Smith", "Brooklyn, New York","Superhuman intelligence"))
+        heroes.add(Hero("Jim", "Stephen Vincent Strange", "New York","Mastery of magic"))
+        heroes.add(Hero("John", "Clinton Barton", "Waverly, Iowa","Marksman"))
+        heroes.add(Hero("Mike", "Thor Odinson", "Asgard","Hammer"))
+
+        heroesAdapter.setHeros(heroes as ArrayList<Hero>)
+        heroesRecView.apply {
+            adapter = heroesAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+        heroesRecView = itemView.findViewById(R.id.heroes_rec_view)
+
+        return itemView
     }
 }
