2c0b071a6e6a3a1c2ad9e083e541b2244bb4ced7
abosch-kr <anthony.bosch@kroger.com>
2021-10-15 08:40:22 -0400

    Removed unneeded dependencies

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.marvelapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
