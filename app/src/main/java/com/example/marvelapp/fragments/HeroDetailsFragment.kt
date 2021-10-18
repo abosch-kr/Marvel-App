e032ceea9a42c4a49cc5e4b8a122adaf6a908cf9
abosch-kr <anthony.bosch@kroger.com>
2021-10-18 16:13:01 -0400

    Implemented onViewCreated and companion object

--- app/src/main/java/com/example/marvelapp/fragments/HeroDetailsFragment.kt
+++ app/src/main/java/com/example/marvelapp/fragments/HeroDetailsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.marvelapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.marvelapp.R
+import com.example.marvelapp.models.Hero
 
 class HeroDetailsFragment : Fragment() {
 
@@ -13,5 +16,29 @@ class HeroDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hero_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Get the data being passed in from the RecyclerViewFragment Bundle.
+        val hero: Hero? = arguments?.getParcelable("hero")
+
+        Log.d(TAG, hero!!.name)
+        Log.d(TAG, hero.alterEgo)
+        Log.d(TAG, hero.origin)
+        Log.d(TAG, hero.ability)
+
+        val heroName: TextView = view.findViewById(R.id.txt_hero_name)
+        val heroAlterEgo: TextView = view.findViewById(R.id.txt_hero_alter_ego)
+        val heroOrigin: TextView = view.findViewById(R.id.txt_hero_origin)
+        val heroAbility: TextView = view.findViewById(R.id.txt_hero_ability)
+
+        heroName.text = hero.name
+        heroAlterEgo.text = hero.alterEgo
+        heroOrigin.text = hero.origin
+        heroAbility.text = hero.ability
+    }
+
+    companion object {
+        const val TAG = "HeroDetailsFragment"
     }
 }
