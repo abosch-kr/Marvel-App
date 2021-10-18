0f72a26ac9a0f7b4a227da40ca31816d97eda8dd
abosch-kr <anthony.bosch@kroger.com>
2021-10-18 16:15:35 -0400

    Deleted higher-order function onHeroClick

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.marvelapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.marvelapp.fragments.HeroDetailsFragment
 import com.example.marvelapp.fragments.RecyclerviewFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerviewFragment = RecyclerviewFragment { onHeroClick() }
+        val recyclerviewFragment = RecyclerviewFragment()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.main_frame_layout, recyclerviewFragment)
         fragmentTransaction.commit()
 
         Log.d(TAG, "Exiting onCreate")
-    }
-
-    private fun onHeroClick() {
-        val heroDetailsFragment = HeroDetailsFragment()
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frame_layout, heroDetailsFragment)
-            .addToBackStack(null)
-            .commit()
     }
 
     companion object {
