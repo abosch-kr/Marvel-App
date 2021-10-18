47921f2015bdeba096eeab242282845413b16a3a
abosch-kr <anthony.bosch@kroger.com>
2021-10-18 16:10:50 -0400

    Implemented Parcelable

--- app/src/main/java/com/example/marvelapp/models/Hero.kt
+++ app/src/main/java/com/example/marvelapp/models/Hero.kt
@@ -1,3 +1,19 @@
 package com.example.marvelapp.models
 
-data class Hero (val name: String, val alterEgo: String, val origin: String, val ability: String)
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
+
+@SuppressLint("ParcelCreator")
+data class Hero (val name: String, val alterEgo: String, val origin: String, val ability: String): Parcelable {
+    override fun describeContents(): Int {
+        return -1
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(name)
+        dest?.writeString(alterEgo)
+        dest?.writeString(origin)
+        dest?.writeString(ability)
+    }
+}
