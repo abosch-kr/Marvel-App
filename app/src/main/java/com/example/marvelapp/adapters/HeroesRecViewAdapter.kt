f2391d14cb2aa338211becbd6deebac645ea16fe
abosch-kr <anthony.bosch@kroger.com>
2021-10-20 10:14:46 -0400

    Added comments

--- app/src/main/java/com/example/marvelapp/adapters/HeroesRecViewAdapter.kt
+++ app/src/main/java/com/example/marvelapp/adapters/HeroesRecViewAdapter.kt
@@ -9,11 +9,17 @@ import com.example.marvelapp.R
 import com.example.marvelapp.models.Hero
 import android.util.Log
 
+/**
+ * @param onHeroClickListener
+ * RecyclerView Adapter class that will translate the Hero data into Views
+ */
 class HeroesRecViewAdapter(onHeroClickListener: OnHeroClickListener) : RecyclerView.Adapter<HeroesRecViewAdapter.HeroesViewHolder>() {
     private var heroes = mutableListOf<Hero>()
     private val mOnHeroClickListener = onHeroClickListener
 
     /**
+     * @param view
+     * @param onHeroClickListener
      * Provides a reference to the type of views being used
      */
     inner class HeroesViewHolder(view: View, onHeroClickListener: OnHeroClickListener) : RecyclerView.ViewHolder(view) {
@@ -25,7 +31,9 @@ class HeroesRecViewAdapter(onHeroClickListener: OnHeroClickListener) : RecyclerV
     }
 
     /**
-     * Create new views
+     * @param parent
+     * @param viewType
+     * Creates new views
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroesViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -35,15 +43,24 @@ class HeroesRecViewAdapter(onHeroClickListener: OnHeroClickListener) : RecyclerV
     }
 
     /**
-     * Replace the contents of a view
+     * @param holder
+     * @param position
+     * Replaces the contents of a view
      */
     override fun onBindViewHolder(holder: HeroesViewHolder, position: Int) {
         holder.txtName.text = heroes[position].name
         Log.d(TAG, holder.txtName.text as String)
     }
 
+    /**
+     * @return size of heroes list
+     */
     override fun getItemCount() = heroes.size
 
+    /**
+     * @param updatedHeroes
+     * Updates the mutable list of heroes
+     */
     fun setHeroes(updatedHeroes: ArrayList<Hero>) {
         heroes = updatedHeroes
         Log.d(TAG, "heroes list updated")
