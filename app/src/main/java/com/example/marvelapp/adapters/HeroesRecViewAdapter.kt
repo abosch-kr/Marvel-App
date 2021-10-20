2e2980049913baa2c05264688b29b23767054cf6
abosch-kr <anthony.bosch@kroger.com>
2021-10-20 09:09:14 -0400

    Updated package location

--- app/src/main/java/com/example/marvelapp/adapters/HeroesRecViewAdapter.kt
+++ app/src/main/java/com/example/marvelapp/adapters/HeroesRecViewAdapter.kt
@@ -1,10 +1,11 @@
-package com.example.marvelapp
+package com.example.marvelapp.adapters
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.marvelapp.R
 import com.example.marvelapp.models.Hero
 
 class HeroesRecViewAdapter(onHeroClickListener: OnHeroClickListener) : RecyclerView.Adapter<HeroesRecViewAdapter.HeroesViewHolder>() {
