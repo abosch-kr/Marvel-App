6fbb8363b6b3a196c34933e88316cd68a321be84
abosch-kr <anthony.bosch@kroger.com>
2021-11-10 10:04:32 -0500

    Implemented character details

--- app/src/main/java/com/example/marvelapp/fragment/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/marvelapp/fragment/CharacterDetailsFragment.kt
@@ -7,14 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.marvelapp.R
+import com.example.marvelapp.model.Character
 
 /**
  * A simple [Fragment] subclass.
  * Use the [CharacterDetailsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CharacterDetailsFragment : Fragment() {
+class CharacterDetailsFragment(private val character: Character) : Fragment() {
     private lateinit var name: TextView
     private lateinit var origin: TextView
     private lateinit var species: TextView
@@ -22,11 +24,6 @@ class CharacterDetailsFragment : Fragment() {
     private lateinit var type: TextView
     private lateinit var url: TextView
     private lateinit var image: ImageView
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,7 +34,27 @@ class CharacterDetailsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        findViews(view)
+        setDetails()
+    }
+
+    private fun setDetails() {
+        name.text = character.name
+        origin.text = character.origin.toString()
+        species.text = character.species
+        status.text = character.status
+        type.text = character.type
+        url.text = character.url
+    }
+
+    private fun findViews(view: View) {
+        name = view.findViewById(R.id.txt_character_name)
+        origin = view.findViewById(R.id.txt_character_origin)
+        species = view.findViewById(R.id.txt_character_species)
+        status = view.findViewById(R.id.txt_character_status)
+        type = view.findViewById(R.id.txt_character_type)
+        url = view.findViewById(R.id.txt_character_url)
+        image = view.findViewById(R.id.img_character)
     }
 
     companion object {
@@ -46,6 +63,6 @@ class CharacterDetailsFragment : Fragment() {
          * this fragment using the provided parameters.
          */
         @JvmStatic
-        fun newInstance() = CharacterDetailsFragment()
+        fun newInstance(characters: Character) = CharacterDetailsFragment(characters)
     }
 }
