57f74cbe5ff101e69fa1b07258bf51621886fe9b
abosch-kr <anthony.bosch@kroger.com>
2021-12-09 14:38:50 -0500

    Removed query param

--- app/src/main/java/com/example/marvelapp/MainViewModelFactory.kt
+++ app/src/main/java/com/example/marvelapp/MainViewModelFactory.kt
@@ -7,6 +7,6 @@ import com.example.marvelapp.repository.Repository
 class MainViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
     private val query = "page"
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return SharedViewModel(repository, query) as T
+        return SharedViewModel(repository) as T
     }
 }
