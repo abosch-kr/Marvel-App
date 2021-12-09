73d0c3496f0a5a63afc4523c2be302776544f977
abosch-kr <anthony.bosch@kroger.com>
2021-12-09 14:41:00 -0500

    Modified getCharacters to append the next api call or create an empty list

--- app/src/main/java/com/example/marvelapp/SharedViewModel.kt
+++ app/src/main/java/com/example/marvelapp/SharedViewModel.kt
@@ -1,22 +1,29 @@
 package com.example.marvelapp
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingSource
-import com.example.marvelapp.model.GetCharactersResponse
+import com.example.marvelapp.api.GetCharactersResponse
+import com.example.marvelapp.model.Character
 import com.example.marvelapp.repository.Repository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
-class SharedViewModel(private val repository: Repository, private val query: String): ViewModel() {
-    val mResponse: MutableLiveData<GetCharactersResponse> = MutableLiveData()
+class SharedViewModel(private val repository: Repository): ViewModel() {
+    private val _apiResponse: MutableLiveData<GetCharactersResponse> = MutableLiveData()
+    val mResponse: LiveData<GetCharactersResponse> = _apiResponse
+    private var currentPage = 1
 
     fun getCharacters() {
         viewModelScope.launch {
-            val data: MutableMap<String, String> = HashMap()
-            data[query] = 3.toString()
-            val response = repository.getCharacters(data = data)
-            mResponse.value = response
+            val response = repository.getCharacters(currentPage)
+            currentPage = currentPage.inc()
+            val newList = _apiResponse.value?.results?.toMutableList() ?: mutableListOf()
+            newList.addAll(response.results)
+            val charResponse = GetCharactersResponse(response.info, newList)
+            _apiResponse.value = charResponse
         }
     }
 }
